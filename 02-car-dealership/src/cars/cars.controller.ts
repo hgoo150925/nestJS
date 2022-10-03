eb3b335fae2e8947ab691e3f2ecfc36db54022c1
hgo150925 <hugo.orlando2030@gmail.com>
2022-10-03 18:24:18 -0300

    se agrega @Get() a cars

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
+// https://docs.nestjs.com/controllers
+// Los controladores son responsables de manejar las solicitudes entrantes (request) y devolver las respuestas (response) al cliente.
+
+// http://localhost:3000/cars
 @Controller('cars')
-export class CarsController {}
+export class CarsController {
+  // Con el decorador @Get decimos a Nest que cuando venga una solicitud Get a este endpoint (cars) se ejecute este metodo getAllCars
+  @Get()
+  getAllCars() {
+    return ['Kia', 'Fiat', 'Ford'];
+  }
+}
