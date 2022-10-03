e5bdd2c581db4c1b75fa3a1b8663be1ec3c3cc0a
hgo150925 <hugo.orlando2030@gmail.com>
2022-10-03 17:29:24 -0300

    types

---
+++ 01-intro-typescript/src/bases/01-types.ts
@@ -0,0 +1,7 @@
+export const name = "Ryan Gosling";
+const age: number = 35;
+const isValid: boolean = true;
+
+export const templateString = `Hi ${name}`;  
+
+console.log(name, age, isValid, templateString);
