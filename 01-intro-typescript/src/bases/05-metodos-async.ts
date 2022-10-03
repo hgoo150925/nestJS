d8bff07c7dc4e706437a7ac7d4ef042655e508d7
hgo150925 <hugo.orlando2030@gmail.com>
2022-10-03 17:30:40 -0300

    methods async

---
+++ 01-intro-typescript/src/bases/05-metodos-async.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+
+// Una clase es la representación de un objeto en la vida real
+// La clase me permite crear instancias de esta clase mientras que una interface me permite crear reglas y condiciones pero no crear una instancia de la misma
+export class Pokemon {
+  // Definicion corta de las propiedades de la clase en el constructor
+  constructor(
+    public readonly id: number, // readonly no me permite establecer/cambiar el valor de la propiedad una vez inicializada su valor
+    public name: string,
+    public country: string
+  ) {}
+
+  get imageUrl(): string {
+    // En una clase el this apunta a la instancia de una clase
+    return `https://pokemon.com/${this.id}.jpg`;
+  }
+
+  // Los metodos funciones que tienen acceso a las propiedades y otros metodos
+  scream() {
+    console.log(`${this.name.toUpperCase()}`);
+  }
+  speak() {
+    console.log(`${this.name}, ${this.name}`);
+  }
+
+  // Metodo asincrono
+  async getMoves() {
+    const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/151`);
+    console.log(data.moves);
+    return data.moves;
+  }
+}
+export const meow = new Pokemon(1, 'Meow', 'Argentina');
+
+meow.getMoves();
